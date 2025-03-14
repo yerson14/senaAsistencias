559d6f9a8f15f57d8acb045cfa6fdba1fac745bb
yerson14 <yerson_herrera@soy.sena.edu.co>
2025-03-13 19:01:40 -0500

    nn

--- sena_asistencias/view/partials/header.php
+++ sena_asistencias/view/partials/header.php
@@ -9,14 +9,3 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body class="bg-gray-100">
-<header class="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex justify-between items-center z-50">
-    <!-- Botón del menú hamburguesa -->
-    <button id="menu-toggle" class="text-blue-500 hover:text-blue-700 focus:outline-none">
-        <i class="fas fa-bars text-2xl"></i> <!-- Ícono de menú hamburguesa -->
-    </button>
-
-    <!-- Ícono de cerrar sesión -->
-    <a href="../../controllers/AuthController.php?action=logout" class="text-blue-500 hover:text-blue-700">
-        <i class="fas fa-sign-out-alt text-2xl"></i> <!-- Ícono de cerrar sesión -->
-    </a>
-</header>
